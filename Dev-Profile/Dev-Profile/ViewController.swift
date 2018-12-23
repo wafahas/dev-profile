58d1b86d8df07ff7f3a99e12002faf4899fec29f
wafaa <wafaa.abudawod@gmail.com>
2018-12-23 20:26:56 +0200

    intial project

--- Dev-Profile/Dev-Profile/ViewController.swift
+++ Dev-Profile/Dev-Profile/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
+        
+        devraduiscorner.layer.cornerRadius = 12.0
+        devraduiscorner.layer.borderWidth = 12.0
+        devraduiscorner.layer.borderColor = UIColor.clear.cgColor
+        devraduiscorner.layer.masksToBounds = true
+        devraduiscorner.layer.shadowColor = UIColor.lightGray.cgColor
+        devraduiscorner.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        devraduiscorner.layer.shadowRadius = 2.0
+        devraduiscorner.layer.shadowOpacity = 1.0
+        devraduiscorner.layer.masksToBounds = false
+        devraduiscorner.layer.shadowPath = UIBezierPath(roundedRect: devraduiscorner.bounds, cornerRadius: devraduiscorner.layer.cornerRadius).cgPath
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBOutlet weak var devraduiscorner: UIImageView!
+    
+    
 
 }
 
